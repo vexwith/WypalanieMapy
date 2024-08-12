23b84de86dc6a6a1968a0a430d3a6b29b92bb351
vexwith <wittoma33@wp.pl>
2024-08-12 17:45:12 +0200

    miny upgrade

--- CustomPieces/saper_bomb.gd
+++ CustomPieces/saper_bomb.gd
@@ -11,5 +11,9 @@ func _on_piece_clicked(piece):
 	if piece == self and not Globals.bomb_clicked:
 		if Globals.saper_count < 8:
 			bomb.show()
-			update(5)
 			Globals.bomb_clicked = true
+			await get_tree().create_timer(1.0).timeout
+			owner.get_parent().sfx.stream = load("res://Wavs/explosion-91872.mp3")
+			owner.get_parent().sfx.play()
+			update(5)
+			bomb.hide()
