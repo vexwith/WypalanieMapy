9a57ffeab3ac23c921bf2ee5be648f38a7f7aa88
vexwith <wittoma33@wp.pl>
2025-03-19 19:02:50 +0100

    meta dark blue and fire

---
+++ CustomPieces/kawałek_saper.gd
@@ -0,0 +1,23 @@
+extends Piece
+
+@onready var bomb = $Sprite2D
+@onready var sfx = $SFX
+@onready var uzbrojenie = preload("res://Wavs/00ekran.wav")
+@onready var explosion = preload("res://Wavs/explosion-91872.mp3")
+
+func _ready():
+	super._ready()
+	SignalBus.connect("piece_clicked", _on_piece_clicked)
+	
+	
+func _on_piece_clicked(piece):
+	if piece == self and stage < 5:
+		bomb.show()
+		sfx.stream = uzbrojenie
+		sfx.play()
+		await sfx.finished
+		sfx.stream = explosion
+		sfx.play()
+		bomb.hide()
+		self.stage = 5
+		update(0)
--- Mapa/MetaMap/meta_mapa.gd
+++ Mapa/MetaMap/meta_mapa.gd
@@ -4,8 +4,15 @@ const SAVE_DIR = "user://saves/"
 const SAVE_FILE_NAME = "save_2.json"
 const SECURITY_KEY = "092GSD2"
 
+@onready var flame_scene = preload("res://Mapa/FireMap/flames.tscn")
 @onready var pieces = $Pieces
 @onready var bgm = $AudioStreamPlayer
+@onready var ognik = $Ognik
+@onready var modulation = $CanvasModulate
+@onready var spawn_timer = $SpawnTimer
+@onready var spread_timer = $SpreadTimer
+
+var frame_index = 0
 
 func verify_save_directory(path : String):
 	DirAccess.make_dir_absolute(path)
@@ -34,13 +41,33 @@ func _ready():
 	verify_save_directory(SAVE_DIR)
 	load_data(SAVE_DIR + SAVE_FILE_NAME)
 	
+	SignalBus.connect("piece_clicked", _on_piece_clicked)
+	
 	Globals.detail_mode = true
 	for i in range(len(pieces.get_children())):
 		var piece = pieces.get_child(i)
 		if Globals.map_pieces.values()[i]:
-			pass
+			piece.rim.z_index = 1
+			match i:
+				0: #blue
+					modulation.color = Color(0, 0, 0.647)
+				1: #dark
+					modulation.color = Color.BLACK
+					ognik.dark_mode = true
+					if pieces.get_child(0).clickable:
+						ognik.light.color = Color(0, 0, 0.9)
+						ognik.light.scale = Vector2(2.0, 2.0)
+					else:
+						ognik.light.color.a = 1.0
+				2:
+					var flame = flame_scene.instantiate()
+					piece.add_child(flame)
+					flame.hide()
+					spawn_timer.start()
+						
 		else:
 			piece.clickable = false
+			piece.find_child("Rim2").hide()
 		piece.sprite.modulate.a = 0.7
 		
 	for piece in pieces.get_children():
@@ -51,10 +78,35 @@ func _ready():
 		
 	bgm.play()
 
-
 func _on_audio_stream_player_finished():
 	bgm.play()
 
 
 func _on_exit_button_up():
 	get_tree().change_scene_to_file("res://UI/menu.tscn")
+
+
+func _on_reset_button_up():
+	get_tree().reload_current_scene()
+
+func _on_spawn_timer_timeout():
+	var fire_piece = pieces.get_child(2)
+	if fire_piece.stage < 5:
+		var flame = fire_piece.get_child(5)
+		flame.show()
+		spread_timer.start()
+
+func _on_spread_timer_timeout():
+	var flame = pieces.get_child(2).get_child(5)
+	if flame.visible:
+		flame.hide()
+		pieces.get_child(2).update(1)
+	spawn_timer.start()
+		
+func _on_piece_clicked(piece):
+	if piece == pieces.get_child(2):
+		var fire_piece = pieces.get_child(2)
+		var flame = fire_piece.get_child(5)
+		if flame.visible:
+			flame.hide()
+			fire_piece.update(-2)
