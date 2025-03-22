6c102ba1c5afcecaabec9032f21dd086a5a48b5c
vexwith <wittoma33@wp.pl>
2025-03-22 22:23:47 +0100

    new game text

--- UI/menu.gd
+++ UI/menu.gd
@@ -6,6 +6,14 @@ const SAVE_FILE_NAME = "save_2.json"
 @onready var cursor = preload("res://Ognik/cursor_0.png")
 
 @onready var secret = $Secret
+@onready var shadow = $Shadow
+@onready var label = $Label
+
+var tekst = ["REKSIU OCKNIJ SIĘ", "PRZED WYRUSZENIEM W DROGĘ NALEŻY WYPALIĆ MAPĘ, KTÓRĄ ZNALAZŁEŹ W BUTELCE",
+			 "SPOKOJNIE, NA PEWNO CI SIĘ UDA"]
+var tekst_index = 0
+var tekst_time = 2.0
+var temp_vol
 
 func _ready():
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -5.9)
@@ -55,8 +63,16 @@ func start_new_game():
 		Globals.map_pieces[key] = false
 
 	Globals.kontynuuj = false
-	get_tree().change_scene_to_file("res://game_manager.tscn")
-
+	
+	#new game start text
+	shadow.show()
+	temp_vol = $AudioStreamPlayer.volume_db
+	var tween = get_tree().create_tween().set_parallel().bind_node(self)
+	tween.finished.connect(_on_label_start)
+	tween.tween_property(shadow, "modulate", Color.WHITE, tekst_time)
+	tween.tween_property($AudioStreamPlayer, "volume_db", -15.0, tekst_time)
+	
+	
 func _on_continue_pressed():
 	Globals.kontynuuj = true
 	get_tree().change_scene_to_file("res://game_manager.tscn")
@@ -72,3 +88,22 @@ func _on_exit_pressed():
 
 func _on_secret_button_up():
 	get_tree().change_scene_to_file("res://Mapa/MetaMap/meta_mapa.tscn")
+	
+	
+func _on_label_start():
+	if tekst_index == tekst.size():
+		get_tree().change_scene_to_file("res://game_manager.tscn")
+	else:
+		$AudioStreamPlayer.volume_db = temp_vol
+		$AudioStreamPlayer.stop()
+		label.text = tekst[tekst_index]
+		var tween = get_tree().create_tween().bind_node(self)
+		tween.finished.connect(_on_label_change)
+		tween.tween_property(label, "modulate", Color.WHITE, tekst_time)
+	
+
+func _on_label_change():
+	tekst_index += 1
+	var tween = get_tree().create_tween().bind_node(self)
+	tween.finished.connect(_on_label_start)
+	tween.tween_property(label, "modulate", Color(1.0, 1.0, 1.0, 0.0), 3.0)
